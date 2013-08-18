a808c79322daadc31db92f51665714d519474d70
nishimaru_hayato <hayato.nishimaru@drecom.co.jp>
2013-08-18 22:05:14 +0900

    Eventのバリデーションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_filter :authenticate
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :check_user, only: [:edit, :create, :update, :destory]
+  before_action :check_user, only: [:edit, :update, :destory]
 
   # login check
   def authenticate
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :image,:name,:date,:place,:price
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/system/images/event/missing.jpg", :hash_secret => "longSecretString", :url => "/system/images/event/:hash.:extension"
+
+  validates :name,  :presence => {:message =>'イベント名は必須です。'}
+
 end
