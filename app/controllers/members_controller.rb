a36020c9b8c1930bf3e12b8163599351f900b48d
nishimaru_hayato <hayato.nishimaru@drecom.co.jp>
2013-09-09 03:09:57 +0900

    参加ボタンと不参加ボタンのajax化

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,15 +4,17 @@ class MembersController < ApplicationController
 
   def create
     @member = Member.new(:event_id => params[:event_id], :user_id => session[:user_id],:attend_status => params[:attend_status])
-    @event = Event.find(params[:event_id]) 
+    @event = Event.find(params[:event_id])   
+    debugger
     respond_to do |format|
       if @member.save
+        debugger
 #        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.html { render :partial => 'show_attend_member_body', success: true, notice: 'イベントの更新に成功しました'}
+        format.html { render :partial => 'events/show_form_body', success: true, notice: 'イベントの更新に成功しました'}
         format.json { head :no_content }
 
       else
-        format.html { render :partial => 'show_attend_member_body', success: false, notice: 'イベントの更新に失敗しました' }
+        format.html { render :partial => 'events/show_form_body', success: false, notice: 'イベントの更新に失敗しました' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
 
       end
@@ -20,21 +22,19 @@ class MembersController < ApplicationController
   end
 
   def update_member
-    @member = Member.find(params[:member_id])
+#    @member = Member.find(params[:member_id])
+    @member = Member.where(event_id: params[:event_id], user_id: session[:user_id]).last
     @event = Event.find(params[:event_id])
+    debugger
     is_success = @member.update(:attend_status => params[:attend_status])
 
-#    @member = Member.where(user_id: session[:user_id], event_id: params[:event_id]).last
-#    is_success = @member.update_attribute(:attend_status => 0) 
-#     @member.update_attiribute(:attend_status => params[:attend_status])
     respond_to do |format|
       if is_success
-#        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.html { render :partial => 'show_attend_member_body', success: true, notice: 'イベントの更新に成功しました'}
+        format.html { render  :partial => 'events/show_form_body', success: true, notice: 'イベントの更新に成功しました'}
         format.json { head :no_content }
 
       else
-        format.html { render :partial => 'show_attend_member_body', success: false, notice: 'イベントの更新に失敗しました' }
+        format.html { render :partial => 'events/show_form_body', success: false, notice: 'イベントの更新に失敗しました' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
 
       end
@@ -46,7 +46,6 @@ class MembersController < ApplicationController
 
   	@event_users = User.select(:id, :name).where.not(id: session[:user_id]).where("name LIKE ?","%"+params[:q]+"%")
 #	render partial: 'members_body'
-    p "MEMBER:::::",@event_users
   	if request.xml_http_request?
   		render partial: 'members_body'
   	else
