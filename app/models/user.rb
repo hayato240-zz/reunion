2b6719b9f3b892a23ea02b235dc1b9576e65c729
Nishimaru Hayato <hayato240@Nishimaru-no-MacBook-Air.local>
2013-09-18 02:14:23 +0900

    初めてログインする際に、プロフィール画像取得する時のバグ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  require 'open-uri'
   has_many :study_abroads
   attr_accessible :name, :image, :study_abroads_attributes
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/system/images/event/missing.jpg", :hash_secret => "longSecretString", :url => "/system/images/event/:hash.:extension"
@@ -10,12 +11,23 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
 #      user.image = auth["info"]["image"]
+      save_file(user, auth["info"]["image"])
 
       if user.provider == "facebook"
          user.name = auth["info"]["name"]
       else
          user.name = auth["info"]["nickname"]
       end
+    end
+  end
+end
+
+def save_file(user, url)
+  filename = File.basename(url)
+  open(filename, 'wb') do |file|
+    open(url) do |data|
+#      file.write(data.read)
+      user.image = data
     end
   end
 end
